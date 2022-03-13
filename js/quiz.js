454aead30edf965ca28d9891472536a97dc91cbc
Amir <amir.hackett@gmail.com>
2022-03-13 15:36:54 -0400

    added timer

--- js/quiz.js
+++ js/quiz.js
@@ -1,1 +1,38 @@
-window.alert("working")
+//array of objects inside curly brackets inside array
+
+var questions = [
+    {
+     question: "How do you log to the console in JavaScript?",
+     options: [
+     "console.log", "window.alert","console.writeLine", "console.dir"
+     ],
+     answer: "console.log"
+    },
+
+    {
+     question: "What's Batman's real name", 
+     options: [ 
+         "Bruce", "Peter", "Tony", "Thanos" 
+     ],
+     answer: "Bruce"
+
+    }
+]
+
+var startingTime = 75;
+var time = document.getElementById("time")
+var start = document.querySelector(".startbtn")
+
+//new way to write a function
+function pageLoad(){
+    start.onclick = startQuiz
+}
+function startQuiz() {
+setInterval(function(){
+startingTime--
+time.textContent = startingTime
+},1000)}
+
+
+pageLoad()
+
